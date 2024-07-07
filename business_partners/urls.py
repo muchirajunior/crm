dd7c1a6eee9c000c6d1c0b6511d90f5f8b8e165c
muchirajunior <muchirajunior022@gmail.com>
2024-07-08 00:21:31 +0300

    Update BPs :gear:

--- business_partners/urls.py
+++ business_partners/urls.py
@@ -3,5 +3,6 @@ from business_partners import views
 
 urlpatterns =[
     path('', views.index, name='business_partners'),
-    path('create', views.create_bp, name='create_bp')
+    path('create', views.create_bp, name='create_bp'),
+    path('update/<int:partner_id>/', views.update_business_partner, name='update_business_partner'),
 ]
--- business_partners/views.py
+++ business_partners/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from business_partners.models import BusinessPartner
 from business_partners.forms import BusinessPartnerForm
+from django.contrib import messages
 
 def index(request):
     context ={
@@ -18,4 +19,26 @@ def create_bp(request):
     else:
         form = BusinessPartnerForm()
 
+    return redirect('business_partners')
+
+def update_business_partner(request, partner_id):
+    business_partner = get_object_or_404(BusinessPartner, id=partner_id)
+
+    if request.method == 'POST':
+        business_partner.code = request.POST.get('code')
+        business_partner.name = request.POST.get('name')
+        business_partner.contact_person = request.POST.get('contact_person')
+        business_partner.email = request.POST.get('email')
+        business_partner.phone = request.POST.get('phone')
+        business_partner.address = request.POST.get('address')
+        business_partner.type = request.POST.get('type')
+
+        try:
+            business_partner.save()
+            messages.success(request, "Business Partner updated successfully.")
+        except Exception as e:
+            messages.error(request, f"Error updating Business Partner: {e}")
+
+        return redirect('business_partners')
+    
     return redirect('business_partners')
